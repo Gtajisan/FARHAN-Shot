f99a6b06ec9309d36955c7cc281ef7a59e9d28a9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-10-23 11:52:49 +0600

    Update FARHAN-Shot2.py

--- FARHAN-Shot2.py
+++ FARHAN-Shot2.py
@@ -17,7 +17,7 @@ import statistics
 import csv
 from typing import Dict
 os.system('cls||clear')
-print("""\033[1;32m
+print("""\033[0;104m
 
 ███████╗██████╗░███╗░░██╗░░░░░░░██╗░░░░░░░██╗██╗███████╗██╗
 ██╔════╝██╔══██╗████╗░██║░░░░░░░██║░░██╗░░██║██║██╔════╝██║
