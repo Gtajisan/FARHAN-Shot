d8c679baf48b06e4e5244363b1b3d95132573747
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-10-23 13:36:02 +0600

    Update FARHAN-Shot.py

--- FARHAN-Shot.py
+++ FARHAN-Shot.py
@@ -591,9 +591,9 @@ class Companion:
         return False
 
     def __credentialPrint(self, wps_pin=None, wpa_psk=None, essid=None):
-        print(f"[+] WPS PIN: '{wps_pin}'")
-        print(f"[+] WPA PSK: '{wpa_psk}'")
-        print(f"[+] AP SSID: '{essid}'")
+        print(f"[✔] WIFI WPS PIN: '{wps_pin}'")
+        print(f"[✔] WIFI PASSWORD: '{wpa_psk}'")
+        print(f"[✔] WIFI BSSID: '{essid}'")
 
     def __saveResult(self, bssid, essid, wps_pin, wpa_psk):
         if not os.path.exists(self.reports_dir):
