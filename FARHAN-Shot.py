85587f41eb114e5fc4af4a9d2e865bde0dce3575
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-12-06 20:21:19 +0600

    Update FARHAN-Shot.py

--- FARHAN-Shot.py
+++ FARHAN-Shot.py
@@ -993,13 +993,14 @@ class WiFiScanner:
     def prompt_network(self) -> str:
         os.system('clear')
         print('''
+SIMPLE WIFI HACKING TOOL
 ███████╗██╗░░██╗███╗░░██╗
 ██╔════╝██║░░██║████╗░██║
 █████╗░░███████║██╔██╗██║
 ██╔══╝░░██╔══██║██║╚████║
 ██║░░░░░██║░░██║██║░╚███║
 ╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚══╝\033[1;36mFARHAN-MUH-TASIM 
-                                                    \n\t\t[\033[1;32m*\033[1;37m] NAME        : FARHAN MUH TASIM\n\t\t[\033[1;32m*\033[1;37m] WORK ONLY   : GREEN-COLOR WIFI\n\t\t[\033[1;32m*\033[1;37m] Create  by  : Farhan \n\t\t[\033[1;32m*\033[1;37m] Version     : FARHAN2.0 \n\t\t[\033[1;32m*\033[1;37m] FACEBOOK    : @FARHAN MUH TASIM \n''')
+                                                    \n\t\t[\033[1;32m*\033[1;37m] NAME        : FARHAN MUH TASIM\n\t\t[\033[1;32m*\033[1;37m] WORK ONLY   : GREEN-COLOR WIFI\n\t\t[\033[1;32m*\033[1;37m] AUTHOR  : Farhan \n\t\t[\033[1;32m*\033[1;37m] Version     : FARHAN2.0 \n\t\t[\033[1;32m*\033[1;37m] FACEBOOK    : @FARHAN MUH TASIM \n''')
         networks = self.iw_scanner()
         if not networks:
             ani('[\033[1;31m-\033[1;37m] No WPS networks found!')
